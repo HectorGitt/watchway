7b27aa778d9344d876da2728ba2ba859d50ff747
Deniyi <adeniyi.olaitanhector@yahoo.com>
2026-02-05 01:15:03 +0100

    added tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
   description: "A nationwide crowdsourced registry to document and track the decay of federal and state infrastructure.",
 };
 
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,7 +23,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5DJBV6GV');`}
+        </Script>
+      </head>
       <body className={`${outfit.variable} antialiased selection:bg-primary selection:text-white`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DJBV6GV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Toaster richColors position="top-right" theme="dark" />
       </body>
