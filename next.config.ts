943ec1514cb3591d3e9e66b231995939c8d0342d
Deniyi <adeniyi.olaitanhector@yahoo.com>
2026-03-14 02:30:00 +0100

    feat: add redirect rules for stability.com and Netlify hosts to _redirects file

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	async redirects() {
-		return [
-			{
-				source: "/:path*",
-				has: [
-					{
-						type: "host",
-						value: "://stabilty.com",
-					},
-				],
-				destination: "https://watchway.org*",
-				permanent: true,
-			},
-			{
-				source: "/:path*",
-				has: [
-					{
-						type: "host",
-						value: "watchway.netlify.app",
-					},
-				],
-				destination: "https://watchway.org*",
-				permanent: true,
-			},
-		];
-	},
+	/* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
